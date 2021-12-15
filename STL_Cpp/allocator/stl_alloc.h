fec9f885998f86f87433fe5484a50738c0705931
PeterWright <peterwrighten@gmail.com>
2021-12-15 13:11:03 +0900

    update：stl_alloc

--- STL_Cpp/allocator/stl_alloc.h
+++ STL_Cpp/allocator/stl_alloc.h
@@ -18,3 +18,67 @@
 #ifndef __SGI_STL_INTERNAL_ALLOC_H 
 #define __SGI_STL_INTERNAL_ALLOC_H
 
+#ifdef __SUNPRO_CC
+#   define __PRIVATE public 
+    //重複のアクセス制限宣言は、不意に何かを私有化することを防止する。
+#else
+#   define __PRIVATE private
+#endif
+
+#ifdef __STL_STATIC_TEMPLATE_MEMBER_BUG
+#   define __USE_MALLOC   
+#endif
+
+//これは、Standard Node Allocatorを実行する。C++ Draft Standard及びOriginal STLのAllocatorとはお一致しない。
+//こちらのAllocatorは、異なるポインタタイプをencapsulateすることではなく、実は、たぶんたった一つのポインタタイプしかない。
+//Allocation　primitivesは、個別のオブジェクトに分属され、Original STLのように大きくのスペースを分配されない。
+
+#ifndef __THROW_BAD_ALLOC
+#   if defined(__STL_NO_BAD_ALLOC) || !defined(__STL_USE_EXCEPTIONS)
+#       include<stdio.h>
+#       include<stdlib.h>
+#       define __THROW_BAD_ALLOC fprintf(stderr, "out of memory\n"); exit(1)
+#   else /* 標準の"out-of-memory"操作に従う */
+#       include<new>
+#       define __THROW_BAD_ALLOC throw std::bad_alloc()
+#   endif 
+#endif
+
+#include<stddef.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+#ifndef __RESTRICT
+#   define __RESTRICT
+#endif
+
+#ifdef __STL_THREADS
+#   include<stl_threads.h>
+#   define __NODE_ALLOCATOR_THREADS true
+#   ifdef __STL_SGI_THREADS
+    //
+    extern "C" {
+        extern int __us_rsthread_malloc;
+    }
+        // 
+#   define __NODE_ALLOCATOR_LOCK if (threads && __us_rsthread_malloc) \
+                {_S_node_allocator_lock._M_acquire_lock(); }
+#   define __NODE_ALLOCATOR_UNLOCK if (threads && __us_rsthread_malloc) \
+                {_S_node_allocator_lock._M_release_lock(); }
+#else /* !__STL_SGI_THREADS */
+#   define __NODE_ALLOCATOR_LOCK \
+                { if (threads) _S_node_allocator_lock._M_acquire_lock(); }
+#   define __NODE_ALLOCATOR_UNLOCK \
+                { if (threads) _S_node_allocator_lock._M_release_lock(); }
+#endif
+#else 
+
+// スレッドは不安全
+
+#   define __NODE_ALLOCATOR_LOCK
+#   define __NODE_ALLOCATOR_UNLOCK
+#   define __NODE_ALLOCATOR_THREADS false
+#endif
+
+__STL_BEGIN_NAMESPACE
+
