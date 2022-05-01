9337129706fe4a922b8d89c0f73ab72987a899a6
PeterWright <apple@PeterWrightnoMacBook-Pro.local>
2022-05-01 22:50:45 +0900

    Update lifetime

---
+++ peter_std/src/lifetime.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+struct Foo;
+
+impl Foo {
+    fn mutate_and_share(&mut self) -> &Self { &*self }
+    fn share(&self) {}
+}
+
+fn main() {
+    let mut foo = Foo;
+    let loan = foo.mutate_and_share();
+    foo.share();
+    println!("{:?}", loan);
+}
