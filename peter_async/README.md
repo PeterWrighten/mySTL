b6f26c305717e4b3de83bdff12b1a6600c178976
PeterWrighten <peterwrighten@gmail.com>
2022-05-09 14:25:48 +0900

    Update Async Part

--- peter_async/README.md
+++ peter_async/README.md
@@ -1,3 +1,10 @@
-# How Async works in Rust
+# Async Rust
+
+- [x] [Async Book](https://rust-lang.github.io/async-book/01_getting_started/01_chapter.html)(Wait tgo read again)
+- [ ] [How Async Functions in Traits could work in Rustc](https://blog.theincredibleholk.org/blog/2022/04/18/how-async-functions-in-traits-could-work-in-rustc/)
+- [ ] [Tokio Tutorial](https://tokio.rs/tokio/tutorial)
+
+## How Async Functions in Traits could work in Rustc
+
 
 
---
+++ peter_async/src/main.rs
@@ -0,0 +1,25 @@
+use std::sync::Arc;
+use async_trait::async_trait;
+
+#[async_trait]
+trait AsyncTrait {
+    async fn get_number(&self) -> i32;
+}
+
+#[async_trait]
+impl AsyncTrait for i32 {
+    async fn get_number(&self) -> i32 {
+        *self
+    }
+}
+
+async fn print_the_number(from: Arc<dyn AsyncTrait>) {
+    let number = from.get_number().await;
+    println!("The number is {}", number);
+}
+
+#[tokio::main]
+async fn main() {
+    let number_getter = Arc::new(43);
+    print_the_number(number_getter).await;
+}
